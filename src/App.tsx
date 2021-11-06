510a9b6b5f4075128ff7d0440477ad675a5659e5
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-11-06 17:52:54 -0300

    feat: add page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="rooms/new" element={<NewRoom />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
